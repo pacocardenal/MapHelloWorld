0e288f15d51e9055f3ee86241be89d7c147dcc54
Paco Cardenal <paco.cardenal@talentomobile.com>
2017-04-21 10:16:34 +0200

    Added pins to map

--- MapHelloWorld/ViewController.swift
+++ MapHelloWorld/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         locationManager.startUpdatingLocation()
         
         mapView.delegate = self
+        
+        addSomePinsToMap()
     }
 
     @IBAction func centerButtonClicked(_ sender: UIButton) {
@@ -25,6 +27,18 @@ class ViewController: UIViewController {
         // Span: 1 ~ 111km
         let region = MKCoordinateRegion(center: londonLocation.coordinate, span: MKCoordinateSpanMake(0.2, 0.2))
         self.mapView.setRegion(region, animated: true)
+    }
+    
+    func addSomePinsToMap() {
+        let a1 = MapPin(coordinate: CLLocationCoordinate2D(latitude: 51.5091, longitude: -0.118))
+        a1.title = "Pin 1"
+        a1.subtitle = "Subtitle 1"
+        self.mapView.addAnnotation(a1)
+        
+        let a2 = MapPin(coordinate: CLLocationCoordinate2D(latitude: 51.5092, longitude: -0.119))
+        a2.title = "Pin 2"
+        a2.subtitle = "Subtitle 2"
+        self.mapView.addAnnotation(a2)
     }
     
 }
