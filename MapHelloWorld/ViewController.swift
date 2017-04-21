93eab7b1b4835e999d6d696cac84b1d976e493e0
Paco Cardenal <paco.cardenal@talentomobile.com>
2017-04-21 10:08:44 +0200

    Created MKAnnotation. Implemented mapView viewFor annotation.

--- MapHelloWorld/ViewController.swift
+++ MapHelloWorld/ViewController.swift
@@ -5,13 +5,17 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    var locationManager: CLLocationManager?
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager = CLLocationManager()
-        locationManager?.requestWhenInUseAuthorization()
+        //locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
     }
 
     @IBAction func centerButtonClicked(_ sender: UIButton) {
@@ -22,5 +26,68 @@ class ViewController: UIViewController {
         let region = MKCoordinateRegion(center: londonLocation.coordinate, span: MKCoordinateSpanMake(0.2, 0.2))
         self.mapView.setRegion(region, animated: true)
     }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print(locations.first.debugDescription)
+    }
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    
+    func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+        print("Start loading")
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        print("Finish loading")
+    }
+    
+    func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+        print("Start rendering")
+    }
+    
+    func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool) {
+        print("Finish rendering")
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView: MKAnnotationView?
+        
+        let reuseIdentifier = "myAnnotationView"
+        if let dequeueAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier) {
+            annotationView = dequeueAnnotationView
+            annotationView?.annotation = annotation
+        } else {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        if let annotationView = annotationView {
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "pin")
+        }
+        
+        return annotationView
+    }
+    
+}
+
+class MapPin: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+    }
+    
 }
 
